3e09c9a3870ced35bd4881947ce5c591aa4ae04c
ernesto.alvarado@oracle.com <ealvarad@slc02oyd.us.oracle.com>
2017-05-02 17:12:10 -0700

    se lee todo

--- src/prueba_caca.py
+++ src/prueba_caca.py
@@ -1,12 +1,22 @@
 import sys
 if __name__ == "__main__":
 	lineas = list(sys.stdin)
+	num_lineas=len(lineas)
 	num_cacasos = int(lineas[0])
 	print("el num de cacasos %u" % num_cacasos)
-	nums = [int(x) for x in lineas[1].strip().split(" ")]
-# 	print("caca %s"%(nums))       
-	for linea in lineas[2:]:
-		que = [int(x) for x in linea.strip().split(" ")]
-# 	        print("query act %s"%(que))       
-                mini = min(nums[que[0]:que[1] + 1])
-		print("%s" % mini)
+
+	idx_linea=1
+	for idx_caso in range(num_cacasos):
+		(num_numeros,num_casos)=[int(x) for x in lineas[idx_linea].strip().split(" ")]
+		print("num nums %u num caca %u"%(num_numeros,num_casos))
+		idx_linea+=1
+		mierda=[0]*num_numeros
+		for caso_act in range(num_casos):
+			tipo_cons=lineas[idx_linea][0]
+			if(tipo_cons=="0"):
+				(_,idx_ini,idx_fin,nuevo_val)=[int(x) for x in lineas[idx_linea].strip().split(" ")]
+				print("actualizacion %u-%u de %u"%(idx_ini,idx_fin,nuevo_val))
+			else:
+				(_,idx_ini,idx_fin)=[int(x) for x in lineas[idx_linea].strip().split(" ")]
+				print("actualizacion %u-%u"%(idx_ini,idx_fin))
+			idx_linea+=1
